a594675793d7555ebf8d3c94eb7471d1c7c5a2e2
Lune Gu <1097852609@qq.com>
2020-10-23 17:47:56 +0800

    修改主页

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <p>Input your code here</p>
+        <textarea></textarea>
+        <p>Choose The Account</p>
+        <form>
+          <input type="radio" name='Standard' />
+          <label for='Standard'>Standard</label>
+          <input type="radio" name='Amazon' />
+          <label for='Amazon'>Amazon</label>
+          <input type="radio" name='Loreal' />
+          <label for='Loreal'>L’Oréal</label>
+          <input type="radio" name='OnStar' />
+          <label for='OnStar'>OnStar</label>
+          <br/>
+          <input type='submit' value='Submit'/>
+        </form>
+      </div>
     </div>
   );
 }
